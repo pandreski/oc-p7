db17cc03db0dac7f3a27924a7331f206d288cab1
Pierre ANDRESKI <pierre.andreski@gmail.com>
2022-11-19 20:28:12 -0500

    Fix dropdown close on focus & w3c errors

--- src/js/factory/SearchDropdown.js
+++ src/js/factory/SearchDropdown.js
@@ -1,17 +1,17 @@
 export default class SearchDropdown {
   constructor(dropdown) {
     this.dropdown = dropdown;
-    this.dropdownButton = this.dropdown.querySelector('.dropdown-toggle');
     this.dropdownMenu = this.dropdown.querySelector('.dropdown-menu');
-    this.buttonLabel = this.dropdownButton.querySelector('.button-label');
-    this.identifier = this.dropdown.getAttribute('data-identifier');
-    this.form = this.dropdownButton.querySelector('form');
+    this.buttonLabelWrapper = this.dropdown.querySelector('.dd-btn-label-wrapper');
+    this.buttonLabel = this.dropdown.querySelector('.button-label');
+    this.form = this.dropdown.querySelector('form');
     this.input = this.form.querySelector('input');
   }
 
   // Set equal width between button and menu
   handleShow() {
-    this.dropdownButton.style.width = `${this.dropdownMenu.offsetWidth}px`;
+    this.dropdown.style.width = `${this.dropdownMenu.offsetWidth}px`;
+    this.buttonLabelWrapper.classList.add('open');
     this.buttonLabel.classList.add('d-none');
     this.form.classList.remove('d-none');
     this.input.focus();
@@ -22,6 +22,7 @@ export default class SearchDropdown {
   handleHide() {
     this.form.classList.add('d-none');
     this.buttonLabel.classList.remove('d-none');
-    this.dropdownButton.style.width = '';
+    this.buttonLabelWrapper.classList.remove('open');
+    this.dropdown.style.width = '';
   }
 }
